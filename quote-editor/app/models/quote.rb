9d993ab0938e4537288d45529c0d2646a015d454
Andres Munoz <andrefe91@gmail.com>
2024-03-10 22:57:40 -0500

    Real time CRUD operations with Action cable and Websockets with Turbo Streams

--- quote-editor/app/models/quote.rb
+++ quote-editor/app/models/quote.rb
@@ -3,4 +3,22 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(created_at: :desc)}
+
+  #Remmember, "puppies" can be anything, this is the name of the channel
+
+  #after_create_commit -> { broadcast_prepend_later_to "puppies"}
+  #after_update_commit -> { broadcast_replace_later_to "puppies" }
+  # The broadcast_remove_later_to method does not exist because as the quote gets deleted from
+  # the database, it would be impossible for a background job to retrieve this quote in the database later
+  # to perform the job
+  #after_destroy_commit -> { broadcast_remove_to "puppies" }
+
+  #Above is the short version of this:
+
+  #  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
+
+
+  #Syntactic sugar for the three callbacks from before:
+
+  broadcasts_to ->(quote) { "puppies" }, inserts_by: :prepend
 end
