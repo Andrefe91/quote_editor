8c5b552c935998915ac004fc3b1a6326035e6ad0
Joshua <andrefe91@gmail.com>
2024-03-12 17:58:12 -0500

    Page controller and navbar html with css

--- quote-editor/app/controllers/application_controller.rb
+++ quote-editor/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-
+  #Authenticate the user everywhere in the application, except the sign up/sign in logic page.
+  before_action :authenticate_user!, unless: :devise_controller?
 end
