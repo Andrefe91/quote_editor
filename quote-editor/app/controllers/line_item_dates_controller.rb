8969f8592f6d96f33d197b5ff6eb4fcd7f907127
Andres Munoz <andrefe91@gmail.com>
2024-03-14 22:38:19 -0500

    Line item dates controller actions added with some turbo for the create function

--- quote-editor/app/controllers/line_item_dates_controller.rb
+++ quote-editor/app/controllers/line_item_dates_controller.rb
@@ -1,5 +1,7 @@
 class LineItemDatesController < ApplicationController
   before_action :set_quote
+  before_action :set_line_item_date, only: [ :edit, :update, :destroy ]
+
   def new
     @line_item_date = @quote.line_item_dates.build
   end
@@ -8,7 +10,10 @@ class LineItemDatesController < ApplicationController
     @line_item_date = @quote.line_item_dates.build(line_item_date_params)
 
     if @line_item_date.save
-      redirect_to quote_path(@quote), notice: "Date was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Date was successfully created." }
+        format.turbo_stream { flash.now[:notive] = "Date was successfully created." }
+      end
     else
       render :new, status: :unprocessable_entity
     end
@@ -18,9 +23,17 @@ class LineItemDatesController < ApplicationController
   end
 
   def update
+    if @line_item_date.update(line_item_date_params)
+      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @line_item_date.destroy
+
+    redirect_to quote_path(@quote), notice: "Date was successfully destroyed."
   end
 
   private
@@ -31,5 +44,9 @@ class LineItemDatesController < ApplicationController
 
   def set_quote
     @quote = current_company.quotes.find(params[:quote_id])
+  end
+
+  def set_line_item_date
+    @line_item_date = @quote.line_item_dates.find(params[:id])
   end
 end
