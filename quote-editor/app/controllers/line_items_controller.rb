ddbedd8c39aa3a7c7c9eed94b4a3482eaae9c948
Andres M <andrefe91@gmail.com>
2024-03-17 13:31:39 -0500

    Finish the line_item controller with views and turbo frames working

--- quote-editor/app/controllers/line_items_controller.rb
+++ quote-editor/app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   before_action :set_quote
   before_action :set_line_item_date
+  before_action :set_line_item, only: [:edit, :update, :destroy ]
 
   def new
     @line_item = @line_item_date.line_items.build
@@ -10,7 +11,10 @@ class LineItemsController < ApplicationController
     @line_item = @line_item_date.line_items.build(line_item_params)
 
     if @line_item.save
-      redirect_to quote_path(@quote), notice: "Item was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item was successfully created." }
+        format.turbo_stream { flash.now[:notice] = "Item was successfully created."}
+      end
     else
       render :new, status: :unprocessable_entity
     end
@@ -20,9 +24,23 @@ class LineItemsController < ApplicationController
   end
 
   def update
+    if @line_item.update(line_item_params)
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item was successfully updated" }
+        format.turbo_stream { flash.now[:notice] = "Item was succesfully updated."}
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @line_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: "Item was successfully destroyed." }
+      format.turbo_stream { flash.now[:notice] = "Item was successfully destroyed." }
+    end
   end
 
   private
@@ -37,5 +55,9 @@ class LineItemsController < ApplicationController
 
   def set_line_item_date
     @line_item_date = @quote.line_item_dates.find(params[:line_item_date_id])
+  end
+
+  def set_line_item
+    @line_item =  @line_item_date.line_items.find(params[:id])
   end
 end
