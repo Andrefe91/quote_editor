fbbee2bf5d495ea515bc6df12ef01bea19e21419
Andres M <andrefe91@gmail.com>
2024-03-16 22:53:09 -0500

    Add line_item model, views and controller

--- quote-editor/app/models/line_item_date.rb
+++ quote-editor/app/models/line_item_date.rb
@@ -1,5 +1,6 @@
 class LineItemDate < ApplicationRecord
   belongs_to :quote
+  has_many :line_items, dependent: :destroy
 
   # The uniqueness is scoped, meaning that is enforced in the context of the same quote_id
   validates :date, presence: true, uniqueness: { scope: :quote_id }
